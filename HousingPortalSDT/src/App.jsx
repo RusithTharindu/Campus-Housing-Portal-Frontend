2e115288ecaf17ecfa13c7fe710545fb6141c3f0
Thushan W A R T <rusiththarindu2@gmail.com>
2024-03-13 00:21:05 +0530

    Accept and Reject Buttons Created

--- HousingPortalSDT/src/App.jsx
+++ HousingPortalSDT/src/App.jsx
@@ -1,12 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import AcceptBtn from './components/AcceptBtn'
+import RejectBtn from './components/RejectBtn'
 
 function App() {
   return(
     <>
-      <Login/>
-      {/* <Register/> */}
+      <div className='bg-black h-[100vh] w-[100vw] flex items-center justify-center'>
+        <AcceptBtn/>
+        <RejectBtn/>
+      </div>
     </>
   )
 }
