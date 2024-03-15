3c03d534065465734e30a8d1962175e407e2635a
Rusith Tharindu Thushan <125721398+RusithTharindu@users.noreply.github.com>
2024-03-15 13:21:38 +0530

    Property Cards Created

--- HousingPortalSDT/src/pages/landlordPages/Landlord.jsx
+++ HousingPortalSDT/src/pages/landlordPages/Landlord.jsx
@@ -1,17 +1,22 @@
 import Container from '../../components/container/container';
 import EmptyRegistration from '../../components/EmptyRegistration';
+import PropertyCard from '../../components/PropertyCard'
 
 const Landlord = () => {
     return(
         <Container>
+            
             <div className='w-[100%] h-[100%] flex flex-row items-center justify-center'>
                 <div className='h-[100%] w-[30%] bg-black'>
                     {/*For left side menu*/}
                     
                 </div>
-                <div className='h-[100%] w-[70%] bg-pink flex'>
+                <div className='h-[100%] w-[70%] flex flex-col justify-center items-center'>
                     {/*For other data desplaying*/}
-                    <EmptyRegistration/>
+                    {/* <EmptyRegistration/> */}
+                    <PropertyCard image = '' name = 'Property 1' location ='Pitipana, Homagama'/>
+                    <PropertyCard image = '' name = 'Property 2' location ='Pitipana, Homagama'/>
+                    <PropertyCard image = '' name = 'Property 3' location ='Pitipana, Homagama'/>
                 </div>
             </div>
         </Container>
