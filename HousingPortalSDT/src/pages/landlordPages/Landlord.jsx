3f4100544fc558dc4fabce2bdb6c78a46d5be20c
Thushan W A R T <rusiththarindu2@gmail.com>
2024-04-02 21:25:08 +0530

    landlord dashboard main regions

--- HousingPortalSDT/src/pages/landlordPages/Landlord.jsx
+++ HousingPortalSDT/src/pages/landlordPages/Landlord.jsx
@@ -2,31 +2,29 @@ import Container from '../../components/container/container';
 import EmptyRegistration from '../../components/EmptyRegistration'
 import GloButton from '../../components/GloButton';
 import PropertyCard from '../../components/PropertyCard'
+import profileicon from '../../assets/profile.png'
+import homes from '../../assets/homes.png'
+import Header from '../../components/Header';
+import LandlordLeftLink from '../../components/LandlordLeftLink';
+import LeftIcon1 from '../../assets/homes.png'
+import LeftIcon2 from '../../assets/profile.png'
 
 const Landlord = () => {
     return(
-        <Container>
-            
-            <div className='w-[100%] h-[100%] flex flex-row items-center justify-center'>
-                <div className='h-[100%] w-[30%]  flex flex-col items-end'>
-                    {/*For left side menu*/}
-                    <div className='w-[80%] h-[50px] m- bg-slate-100 rounded-[10px] active:border active:border-1 active-border-y-indigo-500' >
-
-                    </div>
-                    <div className='w-[100%] h-[50px] bg-slate-100 rounded-[10px] active:border active:border-1 active-border-y-indigo-500'>
-
-                    </div>
-                    
+        <div className='h-[90vh] w-[100vw] flex justify-center items-center'>
+            <div className='h-[800px] w-[1400px] border-2 border-[#dfdfdf] flex flex-row rounded-[10px] '>
+                <div className='h-[100%] w-[30%] flex flex-col items-center border-r-2 border-r-[#dfdfdf] rounded-[20px]' >
+                    {/*For left links */}
+                    <p className='mt-[20px] mb-[20px] font-semibold text-[25px]'>Property Management</p>
+                    <LandlordLeftLink name= "Student Requests" onclick = "" icon = {LeftIcon2}/>
+                    <LandlordLeftLink name= "Registered Properties" onclick = "" icon = {LeftIcon1}/>
+                    <GloButton name = "Register a Property"  />
                 </div>
-                <div className='h-[100%] w-[70%] flex flex-col justify-center items-start'>
-                    {/*For other data desplaying*/}
+                <div className='h-[100%] w-[70%] p-[5px] flex flex-col justify-center items-center'>
                     <EmptyRegistration/>
-                    {/* <PropertyCard image = '' name = 'Property 1' location ='Pitipana, Homagama'/>
-                    <PropertyCard image = '' name = 'Property 2' location ='Pitipana, Homagama'/>
-                    <PropertyCard image = '' name = 'Property 3' location ='Pitipana, Homagama'/> */}
                 </div>
             </div>
-        </Container>
+        </div>
     )
 }
 
